ca082fe20bcf17cd62ca2cb0c59213d87acf8794
adel-sbeh <72353925+adel-sbeh@users.noreply.github.com>
2021-04-28 19:31:36 +0300

    Fixed the random string generator

--- compiler/libraries/rand/random.h
+++ compiler/libraries/rand/random.h
@@ -12,11 +12,10 @@ static int mRan(int minVal, int maxVal){
 
 static char* randStr(size_t size, char *mode) {
 
-    char *str = malloc(sizeof(char)*size), *chrSet;
+    char *str = malloc(sizeof(char)*(size + 1)), *chrSet;
 
-    if(getChrIndx(mode, 'N') != -1){
+    if(getChrIndx(mode, 'N') != -1)
         chrSet = apdStr(chrSet, "0123456789");
-    }
 
     if(getChrIndx(mode, 'S') != -1)
         chrSet = apdStr(chrSet, "abcdefghijklmnopqrstuvwxyz");
@@ -25,11 +24,11 @@ static char* randStr(size_t size, char *mode) {
         chrSet = apdStr(chrSet, "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
 
     if (size) {
-        for (size_t n = 0; n < size; n++) {
+        for (size_t n = 0; n <= size; n++) {
             int key = mRan(0, sizeof(chrSet));
             str[n] = chrSet[key];
         }
-        str[size - 1] = '\0';
+        str[size] = '\0';
     }
 
     return str;
