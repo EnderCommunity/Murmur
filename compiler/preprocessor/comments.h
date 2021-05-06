c0d2350f5da0d9221f5d81866a70ca1fd888b3d5
adel-sbeh <72353925+adel-sbeh@users.noreply.github.com>
2021-05-06 17:10:51 +0300

    The preprocessor now can remove one-line comments

--- compiler/preprocessor/comments.h
+++ compiler/preprocessor/comments.h
@@ -12,7 +12,15 @@ FileInfo* chkForCom(FileInfo *curFile){
 
         printf("\n[Debug] Line Content: %s\n[Debug] '//' Index: %d", curFile->currLineCon, indx);
 
-        curFile->currLineCon = getStrPrt(curFile->currLineCon, 0, indx - 1, 1);
+        printf("\n@@BEFORE: %s@@", curFile->currLineCon);
+
+
+        if(indx == 0){
+            (curFile->currLineCon)[0] = '\0';
+        }else
+            curFile->currLineCon = getStrPrt(curFile->currLineCon, 0, indx - 1, 1);
+
+        printf("\n@@AFTER: %s@@", curFile->currLineCon);
 
     }
 
