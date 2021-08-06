2a681896e5461c54b66f2b65ff12387b6cea2289
EnderAdel <72353925+adel-sbeh@users.noreply.github.com>
2021-08-06 12:48:27 +0300

    Debugging

--- compiler/c/compile.c
+++ compiler/c/compile.c
@@ -1,8 +1,19 @@
-#include <windows.h>
-#include <ShellApi.h>
+#if _WIN64 || _WIN32
+    #include <windows.h>
+    #include <ShellApi.h>
 
-void cmplCCode(char *path){ //Compile a C file
+    void cmplCCode(char *path){ //Compile a C file
 
-    ShellExecute(NULL, "open", C_COMPILER_EXECUTABLE_DIR, apdStr(path, apdStr(" -o ", wrkstn.Name)), wrkstn.Path, SW_SHOW);
+        ShellExecute(NULL, "open", C_COMPILER_EXECUTABLE_DIR, apdStr(path, apdStr(" -o ", wrkstn.Name)), wrkstn.Path, SW_SHOW);
 
-}
+    }
+
+#else
+
+    void cmplCCode(char *path){ //Compile a C file (not windows)
+
+        //???
+
+    }
+
+#endif
