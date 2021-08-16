aa6aed35216673fd1ba81f3ea2ab55ba985283bf
EnderAdel <72353925+adel-sbeh@users.noreply.github.com>
2021-08-17 02:42:48 +0300

    Added some new component definitions to the parser

--- compiler/parser/terminal/operators.h
+++ compiler/parser/terminal/operators.h
@@ -43,3 +43,47 @@ void crtAsiOpr(int linId){ //Create an assignment_operator
     isrtPrsTrm("assignment_operator", "", linId);
 
 }
+
+/**
+ *  comparison_operator
+ *      : = <OPERATOR>
+ *      & = <OPERATOR>
+ *      ;
+**/
+
+int isCmpOpr(M_Token tkn){
+
+    M_Token tmp = lokAhd(); //Get the token ahead
+
+    int cnd = (tkn.typ == LEXER_OPERATOR &&
+        strcmp(tkn.val, "=") == 0 &&
+        ((!tmp.__ahd_filEnd) ? ( //If there is a token after this line, do more checks
+
+            /**
+             *  to make sure this is a comparison operator, make sure
+             *  there's no whitespace after the first token.
+            **/
+            (!(tkn.defVal3)) &&
+
+            /**
+             *  also, compare the second operator!
+            **/
+            strcmp(tmp.val, "=") == 0
+        ) : 0)
+    );
+
+    if(!tmp.__ahd_filEnd) //Not the end of the file
+        remTkn(tmp); //Free the token
+
+    if(cnd)
+        skpNxt(); //Don't forget to skip the next token
+
+    return cnd;
+
+}
+
+void crtCmpOpr(int linId){ //Create a comparison_operator
+
+    isrtPrsTrm("comparison_operator", "", linId);
+
+}
