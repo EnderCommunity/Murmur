cd17d3413585a8e21f39a0643c850f0330893d74
EnderAdel <72353925+adel-sbeh@users.noreply.github.com>
2021-09-13 18:33:48 +0300

    Update zones

--- compiler/parser/par.h
+++ compiler/parser/par.h
@@ -864,7 +864,7 @@ void PrsProc(TmpFileStruc FilStruc, FILE *lexFilPtr){
             remTrmCmp(tmpBkp);
 
         }*/
-        else if(curTrmZon < 3 && strcmp(cmp.nam, PARSER_OPERATORS_ZONE) == 0){ // Manage zone operators
+        else if(curTrmZon < 4 && strcmp(cmp.nam, PARSER_OPERATORS_ZONE) == 0){ // Manage zone operators
 
             //curTrmZon:
             //0 - outside
@@ -875,11 +875,11 @@ void PrsProc(TmpFileStruc FilStruc, FILE *lexFilPtr){
             if(strcmp(cmp.cnt, PARSER_GENERAL_END) == 0)
                 isrtPrsNTrm(PARSER_NTERMINAL_ENDDEFINITION, "", cmp.srcLin);
 
-            if(curTrmZon > 2) { // If you're inside a function
+            /*if(curTrmZon > 2) { // If you're inside a function
 
                 //
 
-            }
+            }*/
 
             //
 
